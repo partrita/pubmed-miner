7b224cc17c7fe625347d7e024a1165e3f22519ea
partrita <partrita@users.noreply.github.com>
2026-02-15 06:47:17 +0000

    deploy: d83006f4e82c45b9b57a917b6d612b045d18cc91

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cd104606.js',
+                path_to_root + 'searchindex-f9f4bfb0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
